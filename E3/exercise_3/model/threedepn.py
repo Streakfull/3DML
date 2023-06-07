d145339c0630b878c9cb4a82e6535c7e41ecfde4
Streakfull <youssef.ahmedyoussef98@gmail.com>
2023-06-07 23:37:39 +0000

    finalizes shapenet model

--- E3/exercise_3/model/threedepn.py
+++ E3/exercise_3/model/threedepn.py
@@ -9,16 +9,56 @@ class ThreeDEPN(nn.Module):
         self.num_features = 80
 
         # TODO: 4 Encoder layers
+        conv1 = nn.Conv3d(in_channels=2, out_channels=self.num_features, kernel_size=4,stride=2,padding=1)
+        relu1 = nn.LeakyReLU(negative_slope=0.2)
+        conv2 =  nn.Conv3d(in_channels=self.num_features,out_channels=self.num_features*2, kernel_size=4,stride=2,padding=1)
+        batch2 = nn.BatchNorm3d(self.num_features*2)
+        conv3  =  nn.Conv3d(in_channels=self.num_features*2,out_channels=self.num_features*4, kernel_size=4,stride=2,padding=1)
+        batch3 = nn.BatchNorm3d(self.num_features*4)
+        conv4  =  nn.Conv3d(in_channels=self.num_features*4,out_channels=self.num_features*8, kernel_size=4,stride=1)
+        batch4 = nn.BatchNorm3d(self.num_features*8)
 
+        self.encoder1 = nn.Sequential(conv1,relu1)
+        self.encoder2 = nn.Sequential(conv2,batch2,relu1)
+        self.encoder3 = nn.Sequential(conv3,batch3,relu1)
+        self.encoder4 = nn.Sequential(conv4,batch4,relu1)
         # TODO: 2 Bottleneck layers
+        linear1 = nn.Linear(in_features=self.num_features*8,out_features=self.num_features*8)
+        linear2 = nn.Linear(in_features=self.num_features*8,out_features=self.num_features*8)
+        self.bottleneck = nn.Sequential(linear1,nn.ReLU(),linear2,nn.ReLU())
 
         # TODO: 4 Decoder layers
+        conv1 = nn.ConvTranspose3d(in_channels=self.num_features * 8 * 2,out_channels=self.num_features*4*2,  kernel_size=4,stride=2,padding=1)
+        batch_norm1 = nn.BatchNorm3d(self.num_features * 4 *2)
+        conv2 = nn.ConvTranspose3d(in_channels=self.num_features * 4 * 2,out_channels=self.num_features*2*2,  kernel_size=4,stride=2,padding=1)
+        batch_norm2 = nn.BatchNorm3d(self.num_features * 2 *2)
+        conv3 = nn.ConvTranspose3d(in_channels=self.num_features * 2 * 2,out_channels=self.num_features*2,  kernel_size=4,stride=2,padding=1)
+        batch_norm3 = nn.BatchNorm3d(self.num_features * 2)
+        conv4 = nn.ConvTranspose3d(in_channels=self.num_features * 2,out_channels=1,kernel_size=4,stride=2,padding=1)
+
+        self.decoder1 = nn.Sequential(conv1,batch_norm1,nn.ReLU())
+        self.decoder2 = nn.Sequential(conv2,batch_norm2,nn.ReLU())
+        self.decoder3 = nn.Sequential(conv3,batch_norm3,nn.ReLU())
+        self.decoder4 = nn.Sequential(conv4)
+
+
+
 
     def forward(self, x):
         b = x.shape[0]
-        # Encode
-        # TODO: Pass x though encoder while keeping the intermediate outputs for the skip connections
-        # Reshape and apply bottleneck layers
+
+        x_e1 = self.encoder1(x)
+        x_e2 = self.encoder2(x_e1)
+        x_e3 = self.encoder3(x_e2)
+        x_e4 = self.encoder4(x_e3)
+
+        print(x_e1.shape)
+        print(x_e2.shape)
+        print(x_e3.shape)
+        print(x_e4.shape)
+        # # Encode
+        # # TODO: Pass x though encoder while keeping the intermediate outputs for the skip connections
+        # # Reshape and apply bottleneck layers
         x = x_e4.view(b, -1)
         x = self.bottleneck(x)
         x = x.view(x.shape[0], x.shape[1], 1, 1, 1)
