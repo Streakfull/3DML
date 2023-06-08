fd0120274f2ea5f5efbeb6ad2bb2739ef4dd89ba
Streakfull <youssef.youssef@tum.de>
2023-06-08 14:32:55 +0200

    adds deepsdf model

--- E3/exercise_3/model/deepsdf.py
+++ E3/exercise_3/model/deepsdf.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torch
+from torch.nn.utils import weight_norm as wn
 
 
 class DeepSDFDecoder(nn.Module):
@@ -9,8 +10,32 @@ class DeepSDFDecoder(nn.Module):
         :param latent_size: latent code vector length
         """
         super().__init__()
+        self.latent_size = latent_size
         dropout_prob = 0.2
+        ln1 = wn(nn.Linear(in_features=latent_size+3,out_features=512))
+        ln2 = wn(nn.Linear(in_features=512,out_features=512))
+        ln3 = wn(nn.Linear(in_features=512,out_features=512))
+        ln4 = wn(nn.Linear(in_features=512,out_features=512-latent_size-3))
+        
 
+
+        self.first_half = nn.Sequential(ln1,nn.ReLU(),nn.Dropout(dropout_prob),
+                                   ln2,nn.ReLU(),nn.Dropout(dropout_prob),
+                                   ln3,nn.ReLU(),nn.Dropout(dropout_prob),
+                                   ln4,nn.ReLU(),nn.Dropout(dropout_prob)
+                                   )
+        
+        ln5 = wn(nn.Linear(in_features=512,out_features=512))
+        ln6 = wn(nn.Linear(in_features=512,out_features=512))
+        ln7 = wn(nn.Linear(in_features=512,out_features=512))
+        ln8 = wn(nn.Linear(in_features=512,out_features=512))
+        ln9 = wn(nn.Linear(in_features=512,out_features=1))
+        self.second_half =  nn.Sequential(ln5,nn.ReLU(),nn.Dropout(dropout_prob),
+                                   ln6,nn.ReLU(),nn.Dropout(dropout_prob),
+                                   ln7,nn.ReLU(),nn.Dropout(dropout_prob),
+                                   ln8,nn.ReLU(),nn.Dropout(dropout_prob),
+                                   ln9
+                                   )
         # TODO: Define model
 
     def forward(self, x_in):
@@ -19,4 +44,8 @@ class DeepSDFDecoder(nn.Module):
         :return: B x 1 tensor
         """
         # TODO: implement forward pass
-        return x
+        #batch_size = x_in.shape(0)
+        x_first_half = self.first_half(x_in)
+        concat = torch.cat((x_first_half,x_in),dim=1)
+        output = self.second_half(concat)
+        return output
