7180b048fa2eef53ce2b54e2428b421c2fe31d2a
Streakfull <youssef.youssef@tum.de>
2023-07-05 04:34:25 +0200

    adds transformer & initial base model

---
+++ Project/src/models/base_model.py
@@ -0,0 +1,56 @@
+import os
+from termcolor import colored, cprint
+import torch
+import utils.util as util
+
+# modified from https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix
+class BaseModel():
+    def name(self):
+        return 'BaseModel'
+
+    def initialize(self, opt):
+        self.opt = opt
+        self.is_train= opt.is_train
+
+        if self.is_train:
+            self.save_dir = os.path.join(opt.logs_dir, opt.name, 'checkpoints')
+
+            if not os.path.exists(self.save_dir):
+                os.makedirs(self.save_dir)
+                cprint(f"{self.save_dir} created", "blue")
+
+        self.model_names = []
+        self.epoch_labels = []
+        self.optimizers = []
+
+    def set_input(self, input):
+        self.input = input
+
+    def forward(self):
+        pass
+
+    def backward(self):
+        pass
+
+    def step(self):
+        pass
+
+    def get_image_paths(self):
+        pass
+
+    def save(self, label):
+        pass
+
+    def train(self,label):
+        pass
+
+    def eval(self,label):
+        pass
+
+
+    def tocuda(self, var_names):
+        for name in var_names:
+            if isinstance(name, str):
+                var = getattr(self, name)
+                setattr(self, name, var.cuda(0, non_blocking=True))
+
