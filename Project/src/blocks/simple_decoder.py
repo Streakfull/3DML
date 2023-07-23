dabac35647efefe988dac2bd011aa3d8f174f3db
Streakfull <youssef.ahmedyoussef98@gmail.com>
2023-07-23 20:30:43 +0200

    checkpoint

--- Project/src/blocks/simple_decoder.py
+++ Project/src/blocks/simple_decoder.py
@@ -1,44 +1,29 @@
 import torch
 from torch import nn
+from blocks.residual_block import ResidualBlock
 
-
-class ResBlock(nn.Module):
-    def __init__(self, chan):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Conv3d(chan, chan, 3, padding=1),
-            nn.ReLU(),
-            nn.Conv3d(chan, chan, 3, padding=1),
-            nn.ReLU(),
-            nn.Conv3d(chan, chan, 1)
-        )
-
-    def forward(self, x):
-        return self.net(x) + x
-
-# TODO: Refactor this properly
 class SimpleDecoder(nn.Module):
      def __init__(self):
               super().__init__()
-              self.initial_conv = nn.Conv3d(in_channels=768,out_channels=64, kernel_size=3,padding=1)
-              self.encoded_conv = nn.Sequential(ResBlock(64),ResBlock(64))
+              d_model = configs["d_model"]
+              out_channels = configs["num_pos_embeddings"]
+              self.initial_conv = nn.Conv3d(in_channels=d_model,out_channels=out_channels, kernel_size=3,padding=1)
+              self.encoded_conv = nn.Sequential(ResidualBlock(out_channels),ResidualBlock(out_channels))
               
               self.conv_transpose = nn.Sequential()
               for i in range(3):
-                    self.conv_transpose.append(nn.ConvTranspose3d(in_channels=64,out_channels=64, kernel_size=4, stride=2, padding=1))
+                    self.conv_transpose.append(nn.ConvTranspose3d(in_channels=out_channels,out_channels=out_channels, kernel_size=4, stride=2, padding=1))
                     self.conv_transpose.append(nn.ReLU())
             
             
-              self.conv_transpose4 = nn.ConvTranspose3d(in_channels=64, out_channels=1 , kernel_size=1)
+              self.conv_transpose4 = nn.ConvTranspose3d(in_channels=out_channels, out_channels=1 , kernel_size=1)
 
     
      def forward(self,x):
-            #import pdb;pdb.set_trace()
             x = self.initial_conv(x)
             x = self.encoded_conv(x)
             x = self.conv_transpose(x)
             x = self.conv_transpose4(x)
-            #import pdb;pdb.set_trace()
             return x;
 
     
